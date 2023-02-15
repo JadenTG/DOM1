8faa2185910fe02f9d9afc6d2b2c28da347a5a4c
JadenTG <111470273+JadenTG@users.noreply.github.com>
2023-02-14 17:56:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+var containerOne = document.createElement('div');
+var containerTwo = document.createElement('div');
+
+var newImg1 = document.createElement('img');
+newImg1.src = 'AL.jpg';
+var newTextEl1 = document.createElement('h2');
+var newText1 = document.createTextNode('Ada Lovelace');
+var newImg2 = document.createElement('img');
+newImg2.src = 'AT.jpg';
+var newTextEl2 = document.createElement('h2');
+var newText2 = document.createTextNode('Alan Turing');
+var newImg3 = document.createElement('img');
+newImg3.src = 'CB.jpg';
+var newTextEl3 = document.createElement('h2');
+var newText3 = document.createTextNode('Charles Babbage');
+
+newTextEl1.appendChild(newText1);
+newTextEl2.appendChild(newText2);
+newTextEl3.appendChild(newText3);
+containerOne.appendChild(newImg1)
+containerOne.appendChild(newImg2)
+containerOne.appendChild(newImg3)
+containerOne.appendChild(newTextEl1);
+containerOne.appendChild(newTextEl2);
+containerOne.appendChild(newTextEl3);
+document.body.appendChild(containerOne);
+document.body.appendChild(containerOne);
+document.body.appendChild(containerOne);
+
+var newImg4 = document.createElement('img');
+newImg4.src = 'SJ.jpg';
+var newTextEl4 = document.createElement('h2');
+var newText4 = document.createTextNode('Steve Jobs');
+var newImg5 = document.createElement('img');
+newImg5.src = 'BG.jpg';
+var newTextEl5 = document.createElement('h2');
+var newText5 = document.createTextNode('Bill Gates');
+var newImg6 = document.createElement('img');
+newImg6.src = 'EM.png';
+var newTextEl6 = document.createElement('h2');
+var newText6 = document.createTextNode('Elon Musk');
+
+newTextEl4.appendChild(newText4);
+newTextEl5.appendChild(newText5);
+newTextEl6.appendChild(newText6);
+containerOne.appendChild(newImg4)
+containerOne.appendChild(newImg5)
+containerOne.appendChild(newImg6)
+containerOne.appendChild(newTextEl4);
+containerOne.appendChild(newTextEl5);
+containerOne.appendChild(newTextEl6);
+document.body.appendChild(containerOne);
+document.body.appendChild(containerOne);
+document.body.appendChild(containerOne);
